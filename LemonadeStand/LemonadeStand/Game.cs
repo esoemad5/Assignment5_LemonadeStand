6f2c7f92da669f6ff25b28b750d529be5e2551f2
esoemad5 <esoemad5@gmail.com>
2018-09-25 11:30:48 -0500

    Began turning the outline in Game.cs into very basic code.

--- LemonadeStand/LemonadeStand/Game.cs
+++ LemonadeStand/LemonadeStand/Game.cs
@@ -8,29 +8,35 @@ namespace LemonadeStand
 {
     class Game
     {
+        private int gameLength;
+        public Game()
+        {
+            gameLength = 7;
+        }
         public void StartGame()
         {
             // Display intro/welcome message
+            Console.WriteLine();
             // Default 7 day game. Asking user for game length is not part of the MVP
             //
             // Loop the following:
-            //
-            // Preperation menu:
-            //      Purchase supplies player.inventory.purchase(Object thing);
-            //      Display player's inventory and money
-            //      Define recipie
-            //      Display weather (forecast and actual may be different)
-            //      Help, continue, and end game options
-            //      Use commands like pesants quest? (throw baby)
-            // Start day: Day day = new Day(player.popularity, weather);
-            //      Use player's popularity and the weather to create Customers
-            //      Each one decides to buy or not buy lemonade, customers can buy more than 1 lemonade
-            //      Those who do buy give feedback
-            // End of day:
-            //      Display results:  money made vs money spent that day, and total money left.
-            //      Non-MVP: # of lemonades sold, customer satisfaction rate, customer feedback table
-            // End of loop
-            //
+            while (true)
+            {
+                // Preperation menu:
+                //      Purchase supplies player.inventory.purchase(Object thing);
+                //      Display player's inventory and money
+                //      Define recipie
+                //      Display weather (forecast and actual may be different)
+                //      Help, continue, and end game options
+                //      Use commands like pesants quest? (throw baby)
+                // Start day: Day day = new Day(player.popularity, weather);
+                //      Use player's popularity and the weather to create Customers
+                //      Each one decides to buy or not buy lemonade, customers can buy more than 1 lemonade
+                //      Those who do buy give feedback
+                // End of day:
+                //      Display results:  money made vs money spent that day, and total money left.
+                //      Non-MVP: # of lemonades sold, customer satisfaction rate, customer feedback table
+            break;}
             // End of day 7, quit, or bankrupt: quit. Any post-game fedback/messages/play again options are non-MVP.
             // Ideas for end of game stuff (non-MVP): How much money is left, how much lemonade sold, customer satisfaction, etc...
         }
