4cab1810f58f3dd7f811c0c9713a761140d05c8d
esoemad5 <esoemad5@gmail.com>
2018-09-25 14:05:48 -0500

    Created and wrote most (if not all) of class ValidInput.

---
+++ LemonadeStand/LemonadeStand/ValidInput.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class ValidInput
+    {
+        private string input;
+        public string Input
+        {
+            get => input;
+        }
+        private string description;
+        public string Description
+        {
+            get => description;
+            set => description = value;
+        }
+        public ValidInput(string input)
+        {
+            this.input = input;
+            description = "No description has been written for this command/item";
+        }
+    }
+}
