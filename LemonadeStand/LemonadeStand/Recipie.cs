d97ee4e5eb2b922a6b1f36e8791eb30ec4acd756
esoemad5 <esoemad5@gmail.com>
2018-09-25 15:12:10 -0500

    Created class Recipie.

---
+++ LemonadeStand/LemonadeStand/Recipie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Recipie
+    {
+        private string[] ingredients;
+        private int[] quantity;
+        
+        public Recipie()
+        {
+            ingredients = new string[3] { "Lemons", "Sugar", "Ice" };
+            quantity = new int[3] { 4, 4, 4 };
+
+        }
+    }
+}
