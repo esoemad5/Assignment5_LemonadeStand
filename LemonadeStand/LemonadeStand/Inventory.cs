5f569589a640dfab9b0d089cf8273b4fbd052d7b
esoemad5 <esoemad5@gmail.com>
2018-09-25 20:44:14 -0500

    Worked on Inventory. Added fields, properties, and methods Add and Remove.

--- LemonadeStand/LemonadeStand/Inventory.cs
+++ LemonadeStand/LemonadeStand/Inventory.cs
@@ -8,5 +8,35 @@ namespace LemonadeStand
 {
     class Inventory
     {
+        private List<Lemon> lemons;
+        public int Lemons
+        {
+            get { return lemons.Count; }
+        }
+        private List<Sugar> sugar;
+        public int Sugar
+        {
+            get { return sugar.Count; }
+        }
+        private List<Ice> ice;
+        public int Ice
+        {
+            get { return ice.Count; }
+        }
+        private List<Cup> cups;
+        public int Cups
+        {
+            get { return cups.Count; }
+        }
+
+        public void Add(Item item) // TODO
+        {
+
+        }
+        public void Remove(Item item) // TODO
+        {
+
+        }
+
     }
 }
