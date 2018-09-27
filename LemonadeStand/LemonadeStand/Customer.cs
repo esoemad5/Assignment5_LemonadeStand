4b61f0dcbc9fa1028209e84dee3a3dea9f882315
esoemad5 <esoemad5@gmail.com>
2018-09-27 07:55:33 -0500

    Customer class is fully outlined.

--- LemonadeStand/LemonadeStand/Customer.cs
+++ LemonadeStand/LemonadeStand/Customer.cs
@@ -21,10 +21,11 @@ namespace LemonadeStand
             // Sunny, Cloudy, Rainy adds 20, 30, 40
             // Temperature ranges from 60-100, every 2 degrees adds 1% to the chance
             // Price: each $0.01 over $0.30 removes 1% from the chance, each $0.01 lower increaces chance, up to a 10% increase
-            // Random number 
-            // Lemonade recipe doesn't have to affect customers. In the future, use customer satisfaction to influence player's popularity, which would influence how many customers would visit the stand.
-            //Thats it???
+            // Random number 30 - rand(60) to make chances different for each customer.         
         }
+        // Lemonade recipe doesn't have to affect customers. In the future, use customer satisfaction to influence player's popularity, which would influence how many customers would visit the stand.
+        // Customers could make feedback(new object)?
+
 
 
     }
