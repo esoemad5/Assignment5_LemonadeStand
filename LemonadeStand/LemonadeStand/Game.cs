b8a6032037d830a6c97bec7b994471cb6f51c177
esoemad5 <esoemad5@gmail.com>
2018-09-28 08:21:09 -0500

    Implemented basic multiplayer. Need to change Console.WriteLines so users know whats going on.

--- LemonadeStand/LemonadeStand/Game.cs
+++ LemonadeStand/LemonadeStand/Game.cs
@@ -30,37 +30,59 @@ namespace LemonadeStand
         public void StartGame()
         {
             int numberOfPlayers = startMenu.Display();
-            Player player = new Player();
+            MakePlayers(numberOfPlayers);
             while (currentDay <= gameLength)
             {
                 weather = new Weather();
-                menu = new PreDayMenu(player, store, weather);
-                menu.MainLoop();
-                if (menu.PlayerWantsToQuit)
+                foreach(Player player in players)
                 {
-                    break;
+                    menu = new PreDayMenu(player, store, weather);
+                    menu.MainLoop();
+                    if (menu.PlayerWantsToQuit)
+                    {
+                        break;
+                    }
                 }
-
-                Day day = new Day(player, weather);
-                day.StartDay(customersPerDay);
-
-                player.Stats.Display(currentDay);
-
-                player.Stats.ResetMoneyEarnedToday();
-                player.Stats.ResetMoneySpentToday();
-                Console.WriteLine();
-                Console.WriteLine("Press c to continue.");
-                char cont = 'a';
-                while(cont != 'c')
+                
+                foreach(Player player in players)
                 {
-                    cont = Console.ReadKey().KeyChar;
+                    Day day = new Day(player, weather);
+                    day.StartDay(customersPerDay);
                 }
+                
+                foreach(Player player in players)
+                {
+                    player.Stats.Display(currentDay);
+
+                    player.Stats.ResetMoneyEarnedToday();
+                    player.Stats.ResetMoneySpentToday();
+                    Console.WriteLine();
+                    Console.WriteLine("Press c to continue.");
+                    char cont = 'a';
+                    while (cont != 'c')
+                    {
+                        cont = Console.ReadKey().KeyChar;
+                    }
+                }
+                
                 currentDay++;
             }
             Console.Clear();
-            player.Stats.Display(currentDay);
-            Console.WriteLine();
-            Console.WriteLine("Summer is over, nobody will buy lemonade until next year. Thank you for playing!!!");
+            foreach(Player player in players)
+            {
+                player.Stats.Display(currentDay);
+                Console.WriteLine();
+                Console.WriteLine("Summer is over, nobody will buy lemonade until next year. Thank you for playing!!!");
+                Console.ReadKey();
+            }
+            
+        }
+        private void MakePlayers(int numberOfPlayers)
+        {
+            for(int i = 0; i < numberOfPlayers; i++)
+            {
+                players.Add(new Player());
+            }
         }
     }
 }
