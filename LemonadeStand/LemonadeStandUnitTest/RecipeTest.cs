b5a65f4608ceb2c5e8808ded0f5c0f7ed206c40e
esoemad5 <esoemad5@gmail.com>
2018-10-02 16:01:03 -0500

    Wrote RecipeTest. Remove_Lemon_1AtElement0, used PrivateObject successfully. That class is weird.

--- LemonadeStand/LemonadeStandUnitTest/RecipeTest.cs
+++ LemonadeStand/LemonadeStandUnitTest/RecipeTest.cs
@@ -18,5 +18,46 @@ namespace LemonadeStandUnitTest
             recipe.Add("Hello World!");
 
         }
+
+        [TestMethod]
+        public void Add_Lemon_1AtElement0()
+        {
+            //Arrange
+            Recipe recipe = new Recipe();
+            int expectedResult = 1;
+
+            //Act
+            recipe.Add("LeMon");
+            int result = recipe.Quantities[0];
+
+            //Assert
+            Assert.AreEqual(result, expectedResult);
+
+        }
+
+        [TestMethod]
+        public void Remove_Lemon_1AtElement0()
+        {
+            //Arrange
+            Recipe recipe = new Recipe();
+            PrivateObject obj = new PrivateObject(recipe);
+            int[] testArray = new int[] { 2, 0, 0 };
+            obj.SetField("quantities", testArray);
+            int expectedResult = 1;
+            
+
+            //Act
+            recipe.Remove("LeMon");
+            /* PrivateObject is for manipulating private fields of and object (call it object A).
+             * Changing a field in PrivateObject will change that same field in object A!!!!!
+             * IF YOU DIE IN THE MATRIX, YOU DIE IN REAL LIFE!!!!!!!!!!!!!!!!!
+             */
+            int result = recipe.Quantities[0];
+
+            //Assert
+            Assert.AreEqual(result, expectedResult);
+
+        }
+
     }
 }
